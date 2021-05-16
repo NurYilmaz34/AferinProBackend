d5dadac2f3ef7d044309f833e7ec4e19a48a4f83
Nur Yılmaz <nur-yilmaz1996@hotmail.com>
2021-05-16 18:17:33 +0300

    refactoring

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     // Çıplak class kalmasın.
     // Entities projesindeki Concrete klasörünün içindeki classlarım veri tabanındaki tablolara denk geliyor.
