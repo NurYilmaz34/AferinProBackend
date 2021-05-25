21aacab0ce43bef69ce60f3db5261f39e9c57b33
Nur Yılmaz <nur-yilmaz1996@hotmail.com>
2021-05-25 12:02:56 +0300

    results added

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message):base(success, message)
+        {
+
+        }
+        public T Data { get; }
+    }
+}
